fafefa07dfab11660304610611de46a29a47335b
Federico <federico.valeri@unicam.it>
2022-11-07 16:09:32 +0100

    Generic List Component

--- src/movies/MoviesList.tsx
+++ src/movies/MoviesList.tsx
@@ -1,22 +1,18 @@
-import Loading from "../utils/Loading";
+import GenericList from "../utils/GenericList";
 import Movie from "./Movie";
 import { movieDTO } from "./movies.module";
 import css from "./MoviesList.module.css";
 
 export default function MoviesList(props: moviesListProps) {
-  if (!props.movies) {
-    return <Loading/>
-  } else if (props.movies.length === 0) {
-    return <>There are no movies to display.</>;
-  } else {
-    return (
+  return (
+    <GenericList list={props.movies}>
       <div className={css.div}>
-        {props.movies.map((movie) => (
+        {props.movies?.map((movie) => (
           <Movie {...movie} key={movie.id} />
         ))}
       </div>
-    );
-  }
+    </GenericList>
+  );
 }
 
 interface moviesListProps {
