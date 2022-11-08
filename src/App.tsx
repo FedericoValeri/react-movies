81bc814d89fa1cf01ceabbc0f704e049b46014ac
Federico <federico.valeri@unicam.it>
2022-11-08 11:28:11 +0100

    Route Configuration in an External File

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,18 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import IndexGenres from "./genres/IndexGenres";
 import Menu from "./Menu";
-import { landingPageDTO } from "./movies/movies.module";
-import MoviesList from "./movies/MoviesList";
+import routes from "./route-config";
 
 function App() {
-  const [movies, setMovies] = useState<landingPageDTO>({});
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setMovies({
-        inTheaters: [
-          {
-            id: 1,
-            title: "Spider-Man: Far From Home",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg",
-          },
-          {
-            id: 2,
-            title: "Luca",
-            poster: "https://m.media-amazon.com/images/I/51mcvFjV-aS._AC_.jpg",
-          },
-        ],
-        upcomingReleases: [
-          {
-            id: 3,
-            title: "Soul",
-            poster:
-              "https://i.etsystatic.com/18242346/r/il/54bef3/2802148755/il_570xN.2802148755_7wzt.jpg",
-          },
-        ],
-      });
-    }, 1000);
-    return () => clearTimeout(timerId);
-  });
-
   return (
     <BrowserRouter>
       <Menu />
       <div className="container">
         <Switch>
-          <Route exact path="/">
-            <h3>In Theaters</h3>
-            <MoviesList movies={movies.inTheaters} />
-            <h3>Upcoming Releases</h3>
-            <MoviesList movies={movies.upcomingReleases} />
-          </Route>
-
-          <Route path="/genres">
-            <IndexGenres />
-          </Route>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} exact={route.exact}>
+              <route.component />
+            </Route>
+          ))}
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/movies/LandingPage.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { landingPageDTO } from "./movies.module";
+import MoviesList from "./MoviesList";
+
+export default function LandingPage() {
+  const [movies, setMovies] = useState<landingPageDTO>({});
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMovies({
+        inTheaters: [
+          {
+            id: 1,
+            title: "Spider-Man: Far From Home",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg",
+          },
+          {
+            id: 2,
+            title: "Luca",
+            poster: "https://m.media-amazon.com/images/I/51mcvFjV-aS._AC_.jpg",
+          },
+        ],
+        upcomingReleases: [
+          {
+            id: 3,
+            title: "Soul",
+            poster:
+              "https://i.etsystatic.com/18242346/r/il/54bef3/2802148755/il_570xN.2802148755_7wzt.jpg",
+          },
+        ],
+      });
+    }, 1000);
+    return () => clearTimeout(timerId);
+  });
+
+  return (
+    <>
+      <h3>In Theaters</h3>
+      <MoviesList movies={movies.inTheaters} />
+      <h3>Upcoming Releases</h3>
+      <MoviesList movies={movies.upcomingReleases} />
+    </>
+  );
+}
