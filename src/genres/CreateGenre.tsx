51c8cb95eb736fdbb96d442ffac0e9f871f6d4ce
Federico <federico.valeri@unicam.it>
2022-11-08 15:59:25 +0100

    First Form

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,20 +1,32 @@
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Formik, Field, Form } from "formik";
 import Button from "../utils/Button";
 
 export default function CreateGenre() {
-  const history = useHistory();
-
   return (
     <>
       <h3>Create Genre</h3>
-      <Button
-        onClick={() => {
-          // ... saving in the database
-          history.push("/genres");
+
+      <Formik
+        initialValues={{
+          name: "",
+        }}
+        onSubmit={(value) => {
+          // when the form is posted
+          console.log(value);
         }}
       >
-        Save Changes
-      </Button>
+        <Form>
+          <div className="mb-3">
+            <label htmlFor="name">Name</label>
+            <Field id="name" name="name" className="form-control" />
+          </div>
+          <Button type="submit">Save Changes</Button>
+          <Link className="btn btn-secondary" to="/genres">
+            Cancel
+          </Link>
+        </Form>
+      </Formik>
     </>
   );
 }
