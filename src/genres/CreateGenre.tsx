4800f74345f35870c44b29a8d50ff3f4b85198c1
Federico <federico.valeri@unicam.it>
2022-11-08 20:25:44 +0100

    GenreForm

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,39 +1,17 @@
-import { Link } from "react-router-dom";
-import { Formik, Form } from "formik";
-import Button from "../utils/Button";
-import * as Yup from "yup";
-import TextField from "../forms/TextField";
+import GenreForm from "./GenreForm";
 
 export default function CreateGenre() {
   return (
     <>
       <h3>Create Genre</h3>
-
-      <Formik
-        initialValues={{
-          name: "",
-        }}
-        onSubmit={async value => {
+      <GenreForm
+        model={{ name: "" }}
+        onSubmit={async (value) => {
           // when the form is posted
-          await new Promise(r => setTimeout(r, 1000));
+          await new Promise((r) => setTimeout(r, 1000));
           console.log(value);
         }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .required("This field is required")
-            .firstLetterUppercase(),
-        })}
-      >
-        {(formikProps) => (
-          <Form>
-            <TextField field="name" displayName="Name" />
-            <Button disabled={formikProps.isSubmitting} type="submit">Save Changes</Button>
-            <Link className="btn btn-secondary" to="/genres">
-              Cancel
-            </Link>
-          </Form>
-        )}
-      </Formik>
+      />
     </>
   );
 }
