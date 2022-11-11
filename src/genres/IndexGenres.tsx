f42344fff8db1b9266696e8954e08e6628516d07
Federico <federico.valeri@unicam.it>
2022-11-11 16:04:22 +0100

    Calling WebApi with axios

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,10 +1,24 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { urlGenres } from "../endpoint";
+import { genreDTO } from "./genres.model";
 
 export default function IndexGenres() {
+  useEffect(() => {
+    axios
+      .get(urlGenres)
+      .then((response: AxiosResponse<genreDTO[]>) => {
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <>
       <h3>Genres</h3>
-      <Link className="btn btn-primary" to="/genres/create">Create</Link>
+      <Link className="btn btn-primary" to="/genres/create">
+        Create
+      </Link>
     </>
   );
 }
