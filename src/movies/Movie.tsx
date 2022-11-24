513c864b38fbfe42d180ba56ce9c34a161192e48
Federico <federico.valeri@unicam.it>
2022-11-24 16:02:12 +0100

    Delete Movie

--- src/movies/Movie.tsx
+++ src/movies/Movie.tsx
@@ -1,10 +1,21 @@
 import { movieDTO } from "./movies.model";
 import css from "./movie.module.css";
 import { Link } from "react-router-dom";
+import Button from "../utils/Button";
+import customConfirm from "../utils/customConfirm";
+import axios from "axios";
+import { urlMovies } from "../endpoint";
+import { useContext } from "react";
+import AlertContext from "../utils/AlertContext";
 
 export default function Movie(props: movieDTO) {
   const buildLink = () => `/movie/${props.id}`;
-
+  const customAlert = useContext(AlertContext);
+  function deleteMovie() {
+    axios.delete(`${urlMovies}/${props.id}`).then(() => {
+      customAlert();
+    });
+  }
   return (
     <div className={css.div}>
       <Link to={buildLink()}>
@@ -13,6 +24,21 @@ export default function Movie(props: movieDTO) {
       <p>
         <Link to={buildLink()}>{props.title}</Link>
       </p>
+      <div>
+        <Link
+          to={`/movies/edit/${props.id}`}
+          style={{ marginRight: "1rem" }}
+          className="btn btn-info"
+        >
+          Edit
+        </Link>
+        <Button
+          onClick={() => customConfirm(() => deleteMovie())}
+          className="btn btn-danger"
+        >
+          Delete
+        </Button>
+      </div>
     </div>
   );
 }
